2dd3c9fadb033fe754ee0390d7b6a0a061ac6c06
Xscrypto001 <ikinyapeter93@gmail.com>
2025-12-05 20:29:06 +0300

    feat: straknet cairo , evm sol generated age verifiers

--- build/Verifier.sol
+++ build/Verifier.sol
@@ -1,22 +1,3 @@
-// SPDX-License-Identifier: GPL-3.0
-/*
-    Copyright 2021 0KIMS association.
-
-    This file is generated with [snarkJS](https://github.com/iden3/snarkjs).
-
-    snarkJS is a free software: you can redistribute it and/or modify it
-    under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    snarkJS is distributed in the hope that it will be useful, but WITHOUT
-    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-    or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public
-    License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with snarkJS. If not, see <https://www.gnu.org/licenses/>.
-*/
 
 pragma solidity >=0.7.0 <0.9.0;
 
@@ -113,7 +94,7 @@ contract Groth16Verifier {
                 mstore(add(_pPairing, 32), mod(sub(q, calldataload(add(pA, 32))), q))
 
                 // B
-                mstore(add(_pPairing, 64), calldataload(pB))
+                mstore(add(_pPairing, 64), calldataload(pB))sncast account create --name my-account --network testnet
                 mstore(add(_pPairing, 96), calldataload(add(pB, 32)))
                 mstore(add(_pPairing, 128), calldataload(add(pB, 64)))
                 mstore(add(_pPairing, 160), calldataload(add(pB, 96)))
